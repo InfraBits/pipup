d3b72456cffc64c88ac37e29b4baad3aa29fc30f
Damian Zaremba <damian@damianzaremba.co.uk>
2022-06-05 12:54:48 +0100

    Fix path typo - we need to access from init

--- pipup/updater.py
+++ pipup/updater.py
@@ -51,7 +51,7 @@ class Updater:
 
             logger.info(f'Discovered: {requirements}')
             self._requirements.append(
-                Requirements.parse_requirements_txt(path, requirements)
+                Requirements.parse_requirements_txt(self._path, requirements)
             )
         return self._requirements
 
